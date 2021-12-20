90a6fa84659f4711de9b2a8038d37bb69f56891b
mariru27 <marina.rusu@caphyon.com>
2021-12-20 12:18:33 +0200

    rename file extenstion to .cs

--- script.ps1
+++ script.ps1
@@ -1,3 +1,3 @@
 
- 1..25 | % {if($_ -gt 9) { MD "Day_$_"; NI "Day_$_/part1.cpp", "Day_$_/part2.cpp"} else { MD "Day_0$_"; NI "Day_0$_/part1.cpp", "Day_0$_/part2.cpp"}}
+ 1..25 | % {if($_ -gt 9) { MD "Day_$_"; NI "Day_$_/part1.cs", "Day_$_/part2.cs"} else { MD "Day_0$_"; NI "Day_0$_/part1.cs", "Day_0$_/part2.cs"}}
  
